ab0d22dce67d07427c59db9ed9f1d078d8cc6aa9
manishrajora25 <manishrajora453@gmail.com>
2025-08-02 22:51:48 +0530

    ssmaind

--- Backend/controllers/userControll.js
+++ Backend/controllers/userControll.js
@@ -65,7 +65,7 @@ export const loginUser = async (req, res) => {
       .cookie("userToken", userToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "none",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .send({
@@ -88,7 +88,7 @@ export const logoutUser = async (req, res) => {
     res.clearCookie(`userToken`, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "None",
       path: "/",
     });
 
