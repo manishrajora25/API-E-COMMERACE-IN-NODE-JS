b957b979197ab3a4d1f03c84f9558d60996364a0
manishrajora25 <manishrajora453@gmail.com>
2025-08-13 16:20:26 +0530

    aamain

--- htmljava/script.js
+++ htmljava/script.js
@@ -1,8 +1,28 @@
-async function name1(params) {
-     const res = await fetch("https://api-e-commerace-in-node-js.onrender.com/poduct/all")
-     const result = await res.json();
-     console.log(result);
+// async function name1() {
+//      const res = await fetch("https://api-e-commerace-in-node-js.onrender.com/product/all")
+//      const result = await res.json();
+//      console.log(result);
      
 
+// }
+// name1();
+
+
+
+async function name1() {
+    try {
+        const res = await fetch("https://api-e-commerace-in-node-js.onrender.com/product/all");
+        withCredential:true;
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        const result = await res.json();
+        console.log(result); // Backend ka pura response
+    } catch (error) {
+        console.error("Error fetching products:", error);
+    }
 }
+
 name1();
